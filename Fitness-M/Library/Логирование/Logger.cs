0ec5f8276134accffe39a261d904c21393f9d7bc
enegriy <nei86@mail.ru>
2014-04-15 22:40:02 +0400

    Изменено логирование брони

---
+++ Fitness-M/Library/Логирование/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fitness_M
+{
+    public static class Logger
+    {
+        private const string m_FileName = "Log.txt";
+
+        public static void Write(string text)
+        {
+            StreamWriter writter = new StreamWriter(m_FileName, true);
+            try
+            {
+                writter.Write(text);
+                writter.Close();
+            }
+            catch
+            {
+                writter.Dispose();
+            }
+        }
+
+        public static void WriteLine(string text)
+        {
+            StreamWriter writter = new StreamWriter(m_FileName, true);
+            try
+            {
+                writter.WriteLine(text);
+                writter.Close();
+            }
+            catch
+            {
+                writter.Dispose();
+            }
+        }
+
+    }
+}
